b2ee404de5810c3da82578ebfbdc5a0b4a75e893
Julius Junior Kazibwe <kazibwejuliusjunior@gmail.com>
2019-05-15 10:33:18 +0300

    contactus form

--- resources/views/emails/contactUS.blade.php
+++ resources/views/emails/contactUS.blade.php
@@ -16,41 +16,41 @@
 @endif
 
 {!! Form::open(['route'=>'contactus.store']) !!}
-
-<div class="form-group {{ $errors->has('first_name') ? 'has-error' : '' }}">
-{!! Form::label('First Name:') !!}
+<div class="row">
+<div class="form-group col-sm-6 {{ $errors->has('first_name') ? 'has-error' : '' }}">
+{!! Form::label('First Name') !!}
 {!! Form::text('first_name', old('first_name'), ['class'=>'form-control', 'placeholder'=>'Enter first name']) !!}
 <span class="text-danger">{{ $errors->first('first_name') }}</span>
 </div>
 
-<div class="form-group {{ $errors->has('last_name') ? 'has-error' : '' }}">
-  {!! Form::label('Last Name:') !!}
+<div class="form-group col-sm-6 {{ $errors->has('last_name') ? 'has-error' : '' }}">
+  {!! Form::label('Last Name') !!}
   {!! Form::text('last_name', old('last_name'), ['class'=>'form-control', 'placeholder'=>'Enter last name']) !!}
   <span class="text-danger">{{ $errors->first('last_name') }}</span>
   </div>
 
-<div class="form-group {{ $errors->has('email') ? 'has-error' : '' }}">
-{!! Form::label('Email:') !!}
+<div class="form-group col-sm-6 {{ $errors->has('email') ? 'has-error' : '' }}">
+{!! Form::label('Email') !!}
 {!! Form::text('email', old('email'), ['class'=>'form-control', 'placeholder'=>'Enter Email']) !!}
 <span class="text-danger">{{ $errors->first('email') }}</span>
 </div>
 
-<div class="form-group {{ $errors->has('subject') ? 'has-error' : '' }}">
-  {!! Form::label('Subject:') !!}
+<div class="form-group col-sm-6 {{ $errors->has('subject') ? 'has-error' : '' }}">
+  {!! Form::label('Subject') !!}
   {!! Form::text('subject', old('subject'), ['class'=>'form-control', 'placeholder'=>'Enter subject']) !!}
   <span class="text-danger">{{ $errors->first('subject') }}</span>
   </div>
 
-<div class="form-group {{ $errors->has('message') ? 'has-error' : '' }}">
-{!! Form::label('Message:') !!}
+<div class="form-group col-sm-12 {{ $errors->has('message') ? 'has-error' : '' }}">
+{!! Form::label('Message') !!}
 {!! Form::textarea('message', old('message'), ['class'=>'form-control', 'placeholder'=>'Enter Message']) !!}
 <span class="text-danger">{{ $errors->first('message') }}</span>
 </div>
 
-<div class="form-group">
-<button class="btn btn-success">Contact US!</button>
+<div class="form-group col-sm-12 text-center">
+  <button type="submit" class="btn btn-template-outlined"><i class="fa fa-envelope-o"></i> Send message</button>
 </div>
-
+</div>
 {!! Form::close() !!}
 
 </div>
--- resources/views/main/contactus.blade.php
+++ resources/views/main/contactus.blade.php
@@ -72,43 +72,49 @@
                 </div>
               </div>
               <div class="col-md-8 mx-auto">
-                <form>
-                  <div class="row">
-                    <div class="col-sm-6">
-                      <div class="form-group">
-                        <label for="firstname">First Name</label>
-                        <input id="firstname" type="text" class="form-control">
-                      </div>
-                    </div>
-                    <div class="col-sm-6">
-                      <div class="form-group">
-                        <label for="lastname">Last Name</label>
-                        <input id="lastname" type="text" class="form-control">
-                      </div>
-                    </div>
-                    <div class="col-sm-6">
-                      <div class="form-group">
-                        <label for="email">Email</label>
-                        <input id="email" type="text" class="form-control">
-                      </div>
-                    </div>
-                    <div class="col-sm-6">
-                      <div class="form-group">
-                        <label for="subject">Subject</label>
-                        <input id="subject" type="text" class="form-control">
-                      </div>
-                    </div>
-                    <div class="col-sm-12">
-                      <div class="form-group">
-                        <label for="message">Message</label>
-                        <textarea id="message" class="form-control"></textarea>
-                      </div>
-                    </div>
-                    <div class="col-sm-12 text-center">
-                      <button type="submit" class="btn btn-template-outlined"><i class="fa fa-envelope-o"></i> Send message</button>
-                    </div>
-                  </div>
-                </form>
+                @if(Session::has('success'))
+   <div class="alert alert-success">
+     {{ Session::get('success') }}
+   </div>
+@endif
+
+{!! Form::open(['route'=>'contactus.store']) !!}
+<div class="row">
+<div class="form-group col-sm-6 {{ $errors->has('first_name') ? 'has-error' : '' }}">
+{!! Form::label('First Name') !!}
+{!! Form::text('first_name', old('first_name'), ['class'=>'form-control', 'placeholder'=>'Enter first name']) !!}
+<span class="text-danger">{{ $errors->first('first_name') }}</span>
+</div>
+
+<div class="form-group col-sm-6 {{ $errors->has('last_name') ? 'has-error' : '' }}">
+  {!! Form::label('Last Name') !!}
+  {!! Form::text('last_name', old('last_name'), ['class'=>'form-control', 'placeholder'=>'Enter last name']) !!}
+  <span class="text-danger">{{ $errors->first('last_name') }}</span>
+  </div>
+
+<div class="form-group col-sm-6 {{ $errors->has('email') ? 'has-error' : '' }}">
+{!! Form::label('Email') !!}
+{!! Form::text('email', old('email'), ['class'=>'form-control', 'placeholder'=>'Enter Email']) !!}
+<span class="text-danger">{{ $errors->first('email') }}</span>
+</div>
+
+<div class="form-group col-sm-6 {{ $errors->has('subject') ? 'has-error' : '' }}">
+  {!! Form::label('Subject') !!}
+  {!! Form::text('subject', old('subject'), ['class'=>'form-control', 'placeholder'=>'Enter subject']) !!}
+  <span class="text-danger">{{ $errors->first('subject') }}</span>
+  </div>
+
+<div class="form-group col-sm-12 {{ $errors->has('message') ? 'has-error' : '' }}">
+{!! Form::label('Message') !!}
+{!! Form::textarea('message', old('message'), ['class'=>'form-control', 'placeholder'=>'Enter Message']) !!}
+<span class="text-danger">{{ $errors->first('message') }}</span>
+</div>
+
+<div class="form-group col-sm-12 text-center">
+  <button type="submit" class="btn btn-template-outlined"><i class="fa fa-envelope-o"></i> Send message</button>
+</div>
+</div>
+{!! Form::close() !!}
               </div>
             </div>
           </section>
